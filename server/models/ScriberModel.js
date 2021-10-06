d37af949e855d80403a13091eccabf5deac0e141
LaurenR01 <78819957+LaurenR01@users.noreply.github.com>
2021-10-06 19:59:26 -0400

    Scriber db setup, model, routes

---
+++ server/models/ScriberModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ScriberSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: "Author name required"
+    },
+
+    title: {
+        type: String,
+        trim: true,
+        required: "Please give your entry a title"
+    },
+
+    body: {
+        type: String,
+        trim: true,
+        required: "What's on your mind?",
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Scriber = mongoose.model("Scriber", ScriberSchema);
+
+module.exports = Scriber;
