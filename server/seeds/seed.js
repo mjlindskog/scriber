42978288c2777d83df1250cb23d87ba3d5236630
Ethan Harsh <ethan@ethanharsh.com>
2021-10-10 21:25:02 +0000

    entry seeds

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,11 +1,23 @@
 const db = require('../config/connection');
-const {Entry} = require('../models/ScriberModel');
+const { Entry } = require('../models/Entry');
 const scribeSeeds = require('./scribeSeeds.js');
 
 db.once('open', async () => {
-    await ScriberModel.deleteMany({});
-    await ScriberModel.create(scribeSeeds);
+    try {
+        await Entry.deleteMany({});
+    } catch {
+        console.error('DB Delete Error')
+    }
+    //console.log(scribeSeeds)
 
+    for (const el of scribeSeeds) {
+        console.log(el);
+        try {
+            await Entry.create(el);
+        } catch {
+            console.error(`Error at ${seed}`)
+        }
+    }
 
     console.log('Database seeded');
     process.exit(0);
