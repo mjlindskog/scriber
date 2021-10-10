44c84e7dc2d523bb54d84ab910a6412dfcbc5826
Ethan Harsh <ethan@ethanharsh.com>
2021-10-10 22:35:20 +0000

    connect collections via hash

--- server/seeds/scribeSeeds.js
+++ server/seeds/scribeSeeds.js
@@ -1,30 +1,30 @@
 const seed = [
     {
-        "authors": ["johndoe123"],
+        "authors": ["98ce58ced3b7efdcdfbab848ba7bee389687d37c46d85cededb9d408dab159f8"],
         "title": "First post",
         "body": "The quick brown fox jumps over the lazy dog.",
         "subject": "animals",
     },
     {
-        "authors": ["codemastergeneral"],
+        "authors": ["452e26dd8c857dd7089092ab78af88ca3da03b150045d5a91bd954db89afd118"],
         "title": "Coding",
         "body": "Coding is the coolest",
         "subject": "technology"
     },
     {
-        "authors": ["genericusername789"],
+        "authors": ["6a695da8e0494f45fee096378780242c9ab7ce7f65feed5aabe7331752bc6eb2"],
         "title": "Generic Title",
         "body": "Generic Body",
         "subject": "general"
     },
     {
-        "authors": ["scriberforever"],
+        "authors": ["59090b666ca3db64f1373cb552dd90a8d1a4ad5688983560421342fe849f6dcc"],
         "title": "I Love Scriber",
         "body": "It's the coolest app in the world.  Love the text to speech functionality!",
         "subject": "technology"
     },
     {
-        "authors": ["gimmethelikes"],
+        "authors": ["1c111a1b91674c5daae46b08d94c4aaf6f0d56ed26b41c649d15701da748d6d2"],
         "title": "Influencer",
         "body": "This account, as with all others, is only for helping convince me that people actually care what I have to say.",
         "subject": "general"
--- server/seeds/userSeeds.js
+++ server/seeds/userSeeds.js
@@ -3,26 +3,31 @@ const userSeeds = [
         'username': 'johndoe123',
         'email': 'johndoe123@gmail.com',
         'password': 'johndoe123',
+        'savedEntries': ["9840a9e655d5b1b624c664fe87575c3829dc9de74736a5b1b12e2ca5f38b9c57"]
     },
     {
         'username': 'codemastergeneral',
         'email': 'codemastergeneral@gmail.com',
         'password': 'codemastergeneral',
+        'savedEntries': ["20605fbdb8ba3465bac316e89592792dfa52c113315154c10eafdcc721866421"]
     },
     {
         'username': 'genericusername789',
         'email': 'genericusername789@gmail.com',
         'password': 'genericusername789',
+        savedEntries: ["f20409b588e91f5f0ebbf661484ac4714288c2d5487bb1892e7c15d10a514809"]
     },
     {
         'username': 'scriberforever',
         'email': 'scriberforever@gmail.com',
         'password': 'scriberforever',
+        'savedEntries': ["e913f2998a1690223531b86a231e5d56ae7239f5ed0a72825d3c6ccc32b73ddb"]
     },
     {
         'username': 'gimmethelikes',
         'email': 'gimmethelikes@gmail.com',
         'password': 'gimmethelikes',
+        'savedEntries': ["e3203eb181eb30453e2b5523ae5140fbe4925daf33e81eda8992264c1bde7097"]
     }
 ]
 
